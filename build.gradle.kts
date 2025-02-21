d999ef0ae41fb406a7d05b6447f4f21972087108
tororo_1066 <panshoku4@gmail.com>
2025-02-21 23:50:17 +0900

    クリーンアップ

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
-import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask.JarUrl
-import groovy.lang.Closure
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("com.github.ben-manes.versions") version "0.41.0"
-    id("dev.s7a.gradle.minecraft.server") version "1.2.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jmailen.kotlinter") version "3.8.0"
 }
-
-val gitVersion: Closure<String> by extra
 
 val pluginVersion: String by project.ext
 val apiVersion: String by project.ext
@@ -21,7 +14,7 @@ java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 repositories {
     mavenCentral()
     maven(url = "https://oss.sonatype.org/content/groups/public/")
-    maven(url = "https://papermc.io/repo/repository/maven-public/")
+    maven(url = "https://repo.papermc.io/repository/maven-public/")
     maven(url = "https://libraries.minecraft.net")
     maven(url = "https://jitpack.io")
     maven {
@@ -48,21 +41,4 @@ tasks.withType<ShadowJar> {
 
 tasks.named("build") {
     dependsOn("shadowJar")
-}
-
-task<LaunchMinecraftServerTask>("buildAndLaunchServer") {
-    val dir = layout.buildDirectory.get().asFile
-    dependsOn("shadowJar")
-    doFirst {
-        copy {
-            from(dir.resolve("libs/${project.name}.jar"))
-            into(dir.resolve("MinecraftServer/plugins"))
-        }
-    }
-
-    jarUrl.set(JarUrl.Paper(pluginVersion))
-    jarName.set("server.jar")
-    serverDirectory.set(dir.resolve("MinecraftServer"))
-    nogui.set(true)
-    agreeEula.set(true)
 }
